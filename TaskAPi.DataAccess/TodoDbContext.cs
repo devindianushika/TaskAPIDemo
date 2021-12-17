a6f4ceca54ffa15e6ae1bc22e80ebaf34ae0c70d
devindianushika <devindianushika@gmail.com>
2021-12-17 16:10:01 +0530

    Added Migrations

---
+++ TaskAPi.DataAccess/TodoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskAPI.Models;
+
+namespace TaskAPi.DataAccess
+{
+   public class TodoDbContext:DbContext
+    {
+
+        DbSet<Todo> Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server =Desktop-33mepoj;Database =TodoDB; User Id =; Password=";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
